1ee7f71b9c69d2f8e0b7f9a3993f6a21574365ea
Kumar Abhishek <kumara1@damcogroup.com>
2017-04-10 13:15:25 +0530

    accordion and slideshow

--- modules/contrib/accordion_blocks/js/accordion-widget.js
+++ modules/contrib/accordion_blocks/js/accordion-widget.js
@@ -1,7 +1,7 @@
 (function($, Drupal) {
     Drupal.behaviors.accordion_blocks = {
         attach: function (context, settings) {
-            $('.accordion_blocks_container', context).accordion({header: "h2.accordion-title", autoHeight: false, collapsible: true});
+            $('.accordion_blocks_container', context).accordion({header: "h2.accordion-title",  icons: false, autoHeight: true, collapsible: true, active : 'none'});
         }
     }
 })(jQuery, Drupal);
